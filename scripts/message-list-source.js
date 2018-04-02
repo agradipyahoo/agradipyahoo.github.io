3b438c2b50b5fb29d328762f588f1cccbb6ebaf6
Agradip Sarkar <Agradip.Sarkar@walmartlabs.com>
2018-04-03 02:15:39 +0530

    sample

---
+++ scripts/message-list-source.js
@@ -0,0 +1,118 @@
+(function(scope) {
+	function timeAgo(datetime,opts){
+		var msMinute = 60 * 1000,
+		msHour = msMinute * 60,
+		msDay = msHour * 24,
+		msMonth = msDay * 30,
+		msYear = msDay * 365;
+		function pluralize(val, str) {
+			return val + ' ' + ((val > 1) ? (str + 's') : str);
+		}    
+		var milli = (new Date(datetime) - new Date()),
+				ms = Math.abs(milli);
+
+			if (ms < msMinute) return 'just now';
+
+			var timeframes = {
+				year: Math.floor(ms / msYear),
+				month: Math.floor((ms % msYear) / msMonth),
+				day: Math.floor((ms % msMonth) / msDay),
+				hour: Math.floor((ms % msDay) / msHour),
+				minute: Math.floor((ms % msHour) / msMinute)
+			};
+
+			var chunks = [], period, val;
+			for (period in timeframes) {
+				val = timeframes[period];
+				chunks.push(pluralize(val, period));
+			}
+
+			// Limit the returned array to return 'max' of non-null segments
+			var compiled = [], i, len = chunks.length,
+				limit = 0, max = opts.max || 10;
+			for (i = 0; i < len; i++) {
+				if (chunks[i] && limit < max) {
+					limit++;
+					compiled.push(chunks[i]);
+				}
+			}
+			var sfx = (opts.ago && milli < 0) ? ' ago' : '';
+			if (opts.and && limit > 1) {
+				if (limit === 2) return compiled.join(' and ') + sfx;
+				compiled[limit - 1] = 'and ' + compiled[limit - 1];
+			}
+
+			return compiled.join(', ') + sfx;
+	}
+
+
+	function MessageListSource() {
+		// Collect template nodes to be cloned when needed.
+		this.tombstone_ = document.querySelector("#templates > .message-item.tombstone");
+		this.messageTemplate_ = document.querySelector("#templates > .message-item:not(.tombstone)");
+		this.nextItem_ = 0;
+	}
+
+	MessageListSource.prototype = {
+		pageToken:'',  
+		fetch: function(count) {
+			// Fetch at least 30 or count more objects for display.
+			count = Math.max(10, count);
+			return new Promise((resolve, reject) => {
+				const xhr = new XMLHttpRequest();
+				const url = `http://message-list.appspot.com/messages?pageToken=${this.pageToken}&limit=${count}`
+				xhr.open("GET", url);
+				xhr.onload = () => resolve(JSON.parse(xhr.responseText));
+				xhr.onerror = () => reject(xhr.statusText);
+				xhr.send();
+			}).then((messagesResponse)=>{
+				this.pageToken = messagesResponse.pageToken;
+				return new Promise((resolve, reject) =>{
+						let items = messagesResponse.messages.map((mesg)=>this.getItem(mesg));
+						resolve(Promise.all(items));
+				});
+			});
+		},
+
+		createTombstone: function() {
+			return this.tombstone_.cloneNode(true);
+		},
+
+		render: function(item, div) {
+			// TODO: Different style?
+			div = div || this.messageTemplate_.cloneNode(true);
+			div.dataset.id = item.id;
+			div.querySelector('.avatar').src =  item.avatar.src ;
+			div.querySelector('.bubble p.body').textContent = item.message;
+			div.querySelector('.bubble .person-name').textContent = item.name;
+			div.querySelector('.bubble .posted-date').textContent = item.time.toString();
+			var img = div.querySelector('.bubble img');
+			return div;
+		},
+		getItem:function(mesg) {
+			const url = 'http://message-list.appspot.com/'
+			return new Promise(function(resolve) {
+				var item = {
+					id: mesg.id,
+					name:mesg.author.name,
+					avatar: url + mesg.author.photoUrl,
+					time: timeAgo(new Date(mesg.updated),{max:1, ago: true,  and: false}),
+					message: mesg.content
+				}
+				var image = new Image();
+					image.src = item.avatar;
+					image.addEventListener('load', function() {
+					item.avatar = image;
+					resolve(item);
+				});
+				image.addEventListener('error', function() {
+					item.avatar = '';
+					resolve(item);
+				});
+			});
+		}
+	};
+	
+	scope.MessageListSource = MessageListSource;
+
+})(self);
---
+++ scripts/swipe.js
@@ -0,0 +1,294 @@
+/**Write polyfill  for request animation frame*/
+window.requestAnimFrame = (function(){
+    'use strict';
+        return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+(function(window){
+    // Simple way to check if some form of pointerevents is enabled or not
+    window.PointerEventsSupport = false;
+    if(window.PointerEvent || window.navigator.msPointerEnabled) {
+      window.PointerEventsSupport = true;
+    }
+       
+
+    /*Updating new position*/
+    function updateTransformProp(element,newValue){
+        
+        var st = window.getComputedStyle(element, null);
+        var tr = st.getPropertyValue("-webkit-transform") ||
+        st.getPropertyValue("-moz-transform") ||
+        st.getPropertyValue("-ms-transform") ||
+        st.getPropertyValue("-o-transform") ||
+        st.getPropertyValue("transform") ||"FAIL";
+        var matrix = new WebKitCSSMatrix(tr);
+        newValue = 'translateY('+matrix.m42+'px)  ' + newValue;
+        return newValue;
+    }
+
+
+    
+    
+
+
+    // Simple way to check if some form of pointerevents is enabled or not
+    window.PointerEventsSupport = false;
+    if(window.PointerEvent || window.navigator.msPointerEnabled) {
+      window.PointerEventsSupport = true;
+    }
+    /* // [END pointereventsupport] */
+    var substriberList = [];
+    function SwipeRevealItem(element) {
+      'use strict';
+
+      // Gloabl state variables
+      var STATE_DEFAULT = 1;
+      var STATE_LEFT_SIDE = 2;
+      var STATE_RIGHT_SIDE = 3;
+
+      var swipeFrontElement = element;
+      var rafPending = false;
+      var initialTouchPos = null;
+      var lastTouchPos = null;
+      var currentXPosition = 0;
+      var currentState = STATE_DEFAULT;
+      var handleSize = 10;
+      
+      // Perform client width here as this can be expensive and doens't
+      // change until window.onresize
+      var itemWidth = swipeFrontElement.clientWidth;
+      var slopValue = itemWidth * (1/4);
+
+      // On resize, change the slop value
+      this.resize = function() {
+        itemWidth = swipeFrontElement.clientWidth;
+        slopValue = itemWidth * (1/4);
+      };
+
+      /* // [START handle-start-gesture] */
+      // Handle the start of gestures
+      this.handleGestureStart = function(evt) {
+        evt.preventDefault();
+
+        if(evt.touches && evt.touches.length > 1) {
+          return;
+        }
+
+        // Add the move and end listeners
+        if (window.PointerEvent) {
+          evt.target.setPointerCapture(evt.pointerId);
+        } else {
+          // Add Mouse Listeners
+          document.addEventListener('mousemove', this.handleGestureMove, true);
+          document.addEventListener('mouseup', this.handleGestureEnd, true);
+        }
+
+        initialTouchPos = getGesturePointFromEvent(evt);
+
+        swipeFrontElement.style.transition = 'initial';
+      }.bind(this);
+      /* // [END handle-start-gesture] */
+
+      // Handle move gestures
+      //
+      /* // [START handle-move] */
+      this.handleGestureMove = function (evt) {
+        evt.preventDefault();
+
+        if(!initialTouchPos) {
+          return;
+        }
+
+        lastTouchPos = getGesturePointFromEvent(evt);
+
+        if(rafPending) {
+          return;
+        }
+
+        rafPending = true;
+
+        window.requestAnimFrame(onAnimFrame);
+      }.bind(this);
+      /* // [END handle-move] */
+
+      /* // [START handle-end-gesture] */
+      // Handle end gestures
+      this.handleGestureEnd = function(evt) {
+        evt.preventDefault();
+
+        if(evt.touches && evt.touches.length > 0) {
+          return;
+        }
+
+        rafPending = false;
+
+        // Remove Event Listeners
+        if (window.PointerEvent) {
+          evt.target.releasePointerCapture(evt.pointerId);
+        } else {
+          // Remove Mouse Listeners
+          document.removeEventListener('mousemove', this.handleGestureMove, true);
+          document.removeEventListener('mouseup', this.handleGestureEnd, true);
+        }
+
+        updateSwipeRestPosition();
+
+        initialTouchPos = null;
+      }.bind(this);
+      /* // [END handle-end-gesture] */
+
+      function updateSwipeRestPosition() {
+        var differenceInX = initialTouchPos.x - lastTouchPos.x;
+        currentXPosition = currentXPosition - differenceInX;
+
+        // Go to the default state and change
+        var newState = STATE_DEFAULT;
+
+        // Check if we need to change state to left or right based on slop value
+        if(Math.abs(differenceInX) > slopValue) {
+          if(currentState === STATE_DEFAULT) {
+            if(differenceInX > 0) {
+              newState = STATE_LEFT_SIDE;
+            } else {
+              newState = STATE_RIGHT_SIDE;
+            }
+          } else {
+            if(currentState === STATE_LEFT_SIDE && differenceInX > 0) {
+              newState = STATE_DEFAULT;
+            } else if(currentState === STATE_RIGHT_SIDE && differenceInX < 0) {
+              newState = STATE_DEFAULT;
+            }
+          }
+        } else {
+          newState = currentState;
+        }
+
+        changeState(newState);
+
+        // swipeFrontElement.style.transition = 'all 150ms ease-out';
+      }
+
+      function changeState(newState) {
+        var transformStyle;
+        switch(newState) {
+          case STATE_DEFAULT:
+            currentXPosition = 0;
+            break;
+          case STATE_LEFT_SIDE:
+            currentXPosition = -(itemWidth - handleSize);
+            break;
+          case STATE_RIGHT_SIDE:
+            currentXPosition = itemWidth - handleSize;
+            break;
+        }
+
+        transformStyle = 'translateX('+currentXPosition+'px)';
+        transformStyle = updateTransformProp(swipeFrontElement,transformStyle);
+        swipeFrontElement.style.msTransform = transformStyle;
+        swipeFrontElement.style.MozTransform = transformStyle;
+        swipeFrontElement.style.webkitTransform = transformStyle;
+        swipeFrontElement.style.transform = transformStyle;
+        swipeFrontElement.style.opacity= 0.3;
+        currentState = newState;
+        if(STATE_RIGHT_SIDE){
+            /**
+             * Event custom event and dispatch
+             */
+            var event = document.createEvent('Event');
+            // Define that the event name is 'build'.
+            event.initEvent('swipeout', true, true);
+            // target can be any Element or other EventTarget.
+            swipeFrontElement.dispatchEvent(event);
+        }
+      }
+
+      function getGesturePointFromEvent(evt) {
+        var point = {};
+
+        if(evt.targetTouches) {
+          point.x = evt.targetTouches[0].clientX;
+          point.y = evt.targetTouches[0].clientY;
+        } else {
+          // Either Mouse event or Pointer Event
+          point.x = evt.clientX;
+          point.y = evt.clientY;
+        }
+
+        return point;
+      }
+
+      /* // [START on-anim-frame] */
+      function onAnimFrame() {
+        if(!rafPending) {
+          return;
+        }
+
+        var differenceInX = initialTouchPos.x - lastTouchPos.x;
+
+        var newXTransform = (currentXPosition - differenceInX)+'px';
+        var transformStyle = 'translateX('+newXTransform+')';
+        transformStyle = updateTransformProp(swipeFrontElement,transformStyle);
+        swipeFrontElement.style.webkitTransform = transformStyle;
+        swipeFrontElement.style.MozTransform = transformStyle;
+        swipeFrontElement.style.msTransform = transformStyle;
+        swipeFrontElement.style.transform = transformStyle;
+
+        rafPending = false;
+      }
+      /* // [END on-anim-frame] */
+
+      /* // [START addlisteners] */
+      // Check if pointer events are supported.
+      if (window.PointerEvent) {
+        // Add Pointer Event Listener
+        swipeFrontElement.addEventListener('pointerdown', this.handleGestureStart, true);
+        swipeFrontElement.addEventListener('pointermove', this.handleGestureMove, true);
+        swipeFrontElement.addEventListener('pointerup', this.handleGestureEnd, true);
+        swipeFrontElement.addEventListener('pointercancel', this.handleGestureEnd, true);
+      } else {
+        // Add Touch Listener
+        swipeFrontElement.addEventListener('touchstart', this.handleGestureStart, true);
+        swipeFrontElement.addEventListener('touchmove', this.handleGestureMove, true);
+        swipeFrontElement.addEventListener('touchend', this.handleGestureEnd, true);
+        swipeFrontElement.addEventListener('touchcancel', this.handleGestureEnd, true);
+
+        // Add Mouse Listener
+        swipeFrontElement.addEventListener('mousedown', this.handleGestureStart, true);
+      }
+      /* // [END addlisteners] */
+    }
+
+    
+
+    function removeTouchEvents(){
+        if (window.PointerEvent) {
+        // Add Pointer Event Listener
+            swipeFrontElement.removeEventListener('pointerdown', this.handleGestureStart, true);
+            swipeFrontElement.removeEventListener('pointermove', this.handleGestureMove, true);
+            swipeFrontElement.removeEventListener('pointerup', this.handleGestureEnd, true);
+            swipeFrontElement.removeEventListener('pointercancel', this.handleGestureEnd, true);
+        } else {
+            // Add Touch Listener
+            swipeFrontElement.removeEventListener('touchstart', this.handleGestureStart, true);
+            swipeFrontElement.removeEventListener('touchmove', this.handleGestureMove, true);
+            swipeFrontElement.removeEventListener('touchend', this.handleGestureEnd, true);
+            swipeFrontElement.removeEventListener('touchcancel', this.handleGestureEnd, true);
+
+            // Add Mouse Listener
+            swipeFrontElement.removeEventListener('mousedown', this.handleGestureStart, true);
+        }
+    
+    }
+
+
+    SwipeRevealItem.prototype = {
+        constructor:SwipeRevealItem,
+        destory:removeTouchEvents
+    }
+    window.SwipeRevealItem = SwipeRevealItem;
+})(self);
