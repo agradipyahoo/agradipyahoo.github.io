41b50e9440aa4858b99c39c43ae0a80273ce3ee1
Agradip Sarkar <Agradip.Sarkar@walmartlabs.com>
2018-04-03 10:31:51 +0530

    sample

--- scripts/swipe.js
+++ scripts/swipe.js
@@ -62,7 +62,7 @@ window.requestAnimFrame = (function(){
       // Perform client width here as this can be expensive and doens't
       // change until window.onresize
       var itemWidth = swipeFrontElement.clientWidth;
-      var slopValue = itemWidth * (1/4);
+      var slopValue = itemWidth * (1/20);
 
       // On resize, change the slop value
       this.resize = function() {
